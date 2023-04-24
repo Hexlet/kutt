127a216693155be3ecb2bbd7078eb2dc49b49e51
Stanislav Dzisiak <stanislav.dzisiak@outlook.com>
2023-04-24 11:52:46 +0300

    fix autofilling fields with creating link

--- client/components/Shortener.tsx
+++ client/components/Shortener.tsx
@@ -299,7 +299,7 @@ const Shortener = () => {
               <TextInput
                 {...text("customurl")}
                 placeholder="Custom address..."
-                autoComplete="off"
+                autoComplete="new-password"
                 data-lpignore
                 pl={[3, 24]}
                 pr={[3, 24]}
@@ -322,7 +322,7 @@ const Shortener = () => {
               <TextInput
                 {...password("password")}
                 placeholder="Password..."
-                autoComplete="off"
+                autoComplete="new-password"
                 data-lpignore
                 pl={[3, 24]}
                 pr={[3, 24]}
